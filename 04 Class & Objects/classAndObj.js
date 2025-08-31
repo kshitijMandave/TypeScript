50bebcd45a873aef90cba892fd6ed4957cfb242b
kshitijMandave <kshitij.mandwe@gmail.com>
2025-08-31 23:31:01 +0530

    Started the Conecept of Class and Objects

---
+++ 04 Class & Objects/classAndObj.js
@@ -0,0 +1,7 @@
+var Device = /** @class */ (function () {
+    function Device() {
+    }
+    return Device;
+}());
+var d1 = new Device();
+var d2 = new Device();
---
+++ 04 Class & Objects/classAndObj.ts
@@ -0,0 +1,8 @@
+class Device {
+  name: "Samsung";
+  category: "Phone";
+  price: 129000;
+}
+
+let d1 = new Device();
+let d2 = new Device();
